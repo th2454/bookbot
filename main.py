71f2164f4f27a29fe2407f03c47cd977511f1f16
Ty Haas <thaas@quenchonline.com>
2026-01-05 14:01:05 -0500

    added sys and sorted dictionary

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_num_words, character_count
+from stats import get_num_words, character_count, chars_dict_to_sorted_list
+import sys
 
 def get_book_text(file_path):
     file_content = None
@@ -6,17 +7,31 @@ def get_book_text(file_path):
         file_content = f.read()
     return file_content
 
+def pretty_report(num_words, sorted_dict, file_path):
+    print('============ BOOKBOT ============')
+    print(f'Analyzing book found at {file_path}...')
+    print('----------- Word Count ----------')
+    print(f'Found {num_words} total words')
+    print('--------- Character Count -------')
+    for dict in sorted_dict:
+        if dict['char'].isalpha():
+            print(f'{dict['char']}: {dict['num']}')
+    print('============= END ===============')
+
 def main():
-    file_path = './books/frankenstein.txt'
+    if len(sys.argv) != 2:
+        print('Usage: python3 main.py <path_to_book>')
+        sys.exit(1)
+    file_path = sys.argv[1]
     file_content = get_book_text(file_path)
 
     num_words = get_num_words(file_content)
 
     char_dict = character_count(file_content)
 
-    print(f'Found {num_words} total words')
+    sorted_dict = chars_dict_to_sorted_list(char_dict)
 
-    print(char_dict)
+    pretty_report(num_words, sorted_dict, file_path)
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -11,3 +11,15 @@ def character_count(file_text):
         else:
             char_dict[t.lower()] = 1
     return char_dict
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+
+    sorted_list.sort(reverse=True, key=sort_on) 
+    
+    return sorted_list
